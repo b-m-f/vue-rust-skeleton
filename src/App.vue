8b47f5871c496b26e63cce9254f4dbb969ea8cba
b-m-f <max@ehlers.berlin>
2020-04-02 23:01:31 +0200

    Adds rust wasm support with wasm-pack and wasm-bindgen. Thanks to https://github.com/rustwasm/wasm-pack-template

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="greet">Greet me</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
+  },
+  methods: {
+    async greet() {
+      const wasm = import("../wasm/pkg");
+      const greet = (await wasm).greet;
+      greet();
+    }
   }
-}
+};
 </script>
 
 <style>
